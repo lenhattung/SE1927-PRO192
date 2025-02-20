10a62efd7a32ca26d498ff9a0d619d9ee4694b11
Tung Le <lenhattung@gmail.com>
2025-02-20 15:51:57 +0700

    ud
    ud

--- Workshop4/src/dto/Vase.java
+++ Workshop4/src/dto/Vase.java
@@ -5,10 +5,54 @@
  */
 package dto;
 
+import java.util.Scanner;
+
 /**
  *
  * @author tungi
  */
-public class Vase extends Item{
+public class Vase extends Item {
+
+    private int height;
+    private String material;
+
+    public Vase() {
+    }
+
+    public Vase(int value, String creator, int height, String material) {
+        super(value, creator);
+        this.height = height;
+        this.material = material;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public String getMaterial() {
+        return material;
+    }
+
+    public void setMaterial(String material) {
+        this.material = material;
+    }
+    public void inputVase(){
+        super.input();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Input height: ");
+        this.height = sc.nextInt();
+        System.out.println("Input material: ");
+        sc = new Scanner(System.in);
+        this.material = sc.nextLine();
+    }
     
+    public void outputVase(){
+        output();
+        System.out.println("Height: " + height);
+        System.out.println("Material: " + material);
+    }
 }
