1280caa553190860d7a5d706a6c149fe7db1b277
Tung Le <lenhattung@gmail.com>
2025-03-17 15:43:13 +0700

    ud
    ud

---
+++ ASM/ASM04/Q4/src/src/IUtilities.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author tungi
+ */
+public interface IUtilities {
+
+    public int checkIntegerNumber(String value, int min, int max);
+
+    public String removeDuplicatedWords(String value);
+    
+    public int checkFormatString(String value);
+
+    public double sumNumbers(String value);
+    
+    public int findFirstOddNumber(String value);
+    
+    public double sumAbsNumbers(String value);
+    
+}
